cfaa4fd6376bf2e9d1261609c0a2e8ccbe39aaf3
AWS ParallelCluster user <ec2-user@ip-10-0-6-151.ap-southeast-1.compute.internal>
2022-10-31 15:41:11 +0000

    Add files-Lab 7

--- fsall-slurm.sh
+++ fsall-slurm.sh
@@ -6,17 +6,19 @@
 #SBATCH --ntasks=1   # number of processor cores (i.e. tasks)
 #SBATCH --nodes=1   # number of nodes
 #SBATCH --cpus-per-task=1       # number of processors per task
-#SBATCH -J "rpllfp"   # job name
+#SBATCH -J "fsall"   # job name
 
 ## /SBATCH -p general # partition (queue)
-#SBATCH -o freq-slurm.%N.%j.out # STDOUT
-#SBATCH -e freq-slurm.%N.%j.err # STDERR
-
+#SBATCH -o fsall-slurm.%N.%j.out # STDOUT
+#SBATCH -e fsall-slurm.%N.%j.err # STDERR
 # LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE
+
+python -u -c "import PyHipp as pyh; \
 import DataProcessingTools as DPT; \
 lfall = DPT.objects.processDirs(dirs=None, exclude=['*eye*', '*mountains*'], objtype=pyh.FreqSpectrum, saveLevel=1); \
 lfall.save(); \
 hfall = DPT.objects.processDirs(dirs=None, exclude=['*eye*', '*mountains*'], objtype=pyh.FreqSpectrum, loadHighPass=True, pointsPerWindow=3000, saveLevel=1); \
-hfall.save();
+hfall.save();"
+
 
 aws sns publish --topic-arn arn:aws:sns:ap-southeast-1:995441325688:awsnotify --message "FSJobDone"
